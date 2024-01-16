1dd907aae74c51dd3544ff5a5e0266d2208ddf34
Muhammad Ali <alihadees786@gmail.com>
2024-01-16 10:37:07 +0500

    Create delete_get_random.py
    Implement the RandomizedSet class:
    
    RandomizedSet() Initializes the RandomizedSet object.
    bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
    bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
    int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
    You must implement the functions of the class such that each function works in average O(1) time complexity.

---
+++ delete_get_random.py
@@ -0,0 +1,28 @@
+import random
+class RandomizedSet:
+
+    def __init__(self):
+        self.data = {}
+        
+    def insert(self, val: int) -> bool:
+        if val in self.data:
+            return False
+        self.data[val] =  1
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val in self.data:
+            del self.data[val]
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        return random.choice(list(self.data.keys()))
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# print(obj, "aaa")
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
